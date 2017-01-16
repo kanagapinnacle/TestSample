bd8d41bd0d1538a7145456a8a3a1b36555f8c57b
kanagapinnacle <kanaga@pinnaclesoft.io>
2017-01-16 15:46:47 -0500

    commit

---
+++ src/main/java/com/example/AvailabilityTest.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import static org.testng.Assert.assertTrue;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class AvailabilityTest extends TestUtil {
+	private static final Log logger = LogFactory.getLog(AvailabilityTest.class);
+	
+	@Test
+	public void availabiltyJson() {
+		String uri = getProperty(Constants.UI_BASE_URL) ;
+		Response response = RestAssured.given().contentType("application/json").when().get(uri).then().extract()
+				.response();
+		// String jsonString =response.getBody().asString();
+		logger.debug(uri);
+		assertTrue((response.statusCode() == 200),
+				"Assertion Failed:Expecting response 200 , Response Code returned is:" + response.statusCode());
+
+	}
+}
---
+++ src/main/java/com/example/TestUtil.java
@@ -0,0 +1,72 @@
+package com.example;
+
+import java.io.FileInputStream;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+
+public class TestUtil  {
+
+	//Initialize variables from Application.properties
+	private static final Log logger = LogFactory.getLog(TestUtil.class);
+	public static String environment;
+	public static Properties p = null;
+	public static Calendar localCalendar;
+	public static Double latitude=null;
+	public static Double longitude =null;
+	public static String localTimeZone=null;
+	public static HashMap<String,String> entitygivenkey = new HashMap<String, String>();
+
+
+	@BeforeSuite
+@Parameters( {"environment"} )
+public void initFramework(@Optional("dev.configuration.properties") String configfile,ITestContext context) throws Exception{
+
+	
+		environment=configfile;
+		String s="in SuiteTestBase";
+		logger.info("In SuiteTestBase.java"+s);
+		init(configfile);
+		//configureProxySettings();
+		addPropertiesToTestContext(context);
+	}
+
+	private void addPropertiesToTestContext(ITestContext context) {
+		for (Map.Entry<Object, Object> property : p.entrySet()) {
+			context.setAttribute((String) property.getKey(), property.getValue());
+		}
+	}
+
+	protected String getProperty(String name) {
+		return getProperty(name, null);
+	}
+
+	protected String getProperty(String name, String defaultValue) {
+		return p.getProperty(name, defaultValue);
+	}
+
+	
+
+
+	 //Load the properties file
+	 public void init(String configFile) {
+	        try {
+	            FileInputStream fis = new FileInputStream(configFile);
+	            p = new Properties();
+	            p.load(fis);
+	        }catch (Exception e) {
+	            logger.info("Exception "+e.getMessage());
+	        }
+	    }
+
+	  
+}
